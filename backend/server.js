7289860ba7212a18035bbaea2b1ddc8ed47cb664
unknown <prae>
2026-02-06 18:56:51 +0700

    update

--- backend/server.js
+++ backend/server.js
@@ -46,6 +46,7 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+const HOST = '0.0.0.0';
+app.listen(PORT, HOST, () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
 });
